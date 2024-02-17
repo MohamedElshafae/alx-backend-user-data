0ceed5a294b31429748b11e340b60cec4c90a642
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-17 02:54:24 +0200

    fix Error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,6 +11,10 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
+        if path and path[-1] != '/':
+            path += '/'
+        if excluded_paths is None or path not in excluded_paths or not path:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
