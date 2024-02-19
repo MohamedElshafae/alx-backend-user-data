bd7089d23bc8e86278893b1c86732d4612d08068
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-19 16:40:26 +0200

    fix styling

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,17 @@
 BasicAuth class
 """
 from api.v1.auth.auth import Auth
-from typing import List, TypeVar
 
 
 class BasicAuth(Auth):
     """BasicAuth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """Returns the Base64 part of the Authorization header for a Basic Authentication"""
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        """
+            Returns the Base64 part of the
+            Authorization header for a Basic Authentication
+        """
         if not authorization_header \
                 or not isinstance(authorization_header, str) \
                 or not authorization_header.startswith('Basic '):
